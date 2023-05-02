a038956d2d2ffebdaa179ce0efe703dab6b212c3
sahba <sahba.ch@gmail.com>
2023-05-02 04:32:19 -0400

    fix cors error

--- organization-backend/app.js
+++ organization-backend/app.js
@@ -10,12 +10,19 @@ const positionRouter = require('./routes/position');
 dotenv.config();
 
 const app = express();
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-});
 app.use(bodyParser.json());
-app.use(cors());
+
+// CORS configuration to allow requests from specific origin
+const allowedOrigins = ['http://34.125.241.148:3000', 'localhost:3000'];
+app.use(cors({
+    origin: function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+}));
 
 app.use('/api/organization', organizationRouter);
 app.use('/api/employees', employeeRouter);
