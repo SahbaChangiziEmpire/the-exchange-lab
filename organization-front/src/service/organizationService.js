d6fef6efb330046261da86023d3af719b7f7e18c
sahba <sahba.ch@gmail.com>
2023-05-02 09:56:25 -0400

    fix the end point configs

--- organization-front/src/service/organizationService.js
+++ organization-front/src/service/organizationService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API_URL = process.env.REACT_NODE_ENV === 'production' ? process.env.REACT_APP_API_URL : process.env.REACT_APP_LOCAL_API_URL;
+const API_URL = process.env.REACT_APP_NODE_ENV === 'production' ? process.env.REACT_APP_API_URL : process.env.REACT_APP_LOCAL_API_URL;
 
 
 export const fetchOrganizationHierarchy = async () => {
